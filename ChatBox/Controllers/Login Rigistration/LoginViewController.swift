f024c7c7fe8209ed87916668c12ff86153d6c42e
harsha <harsh.mundaragi@robosoftin.com>
2023-09-26 10:51:04 +0530

    Completed the log in and registration screen design

--- ChatBox/Controllers/Login Rigistration/LoginViewController.swift
+++ ChatBox/Controllers/Login Rigistration/LoginViewController.swift
@@ -9,16 +9,73 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Log in"
-        view.backgroundColor = .lightGray
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(presentRegisterScreen))
+        emailField.delegate = self
+        passwordField.delegate = self
     }
     
     @objc func presentRegisterScreen(){
         let vc = storyboard?.instantiateViewController(withIdentifier: "RegistrationViewController") as! RegistrationViewController
         navigationController?.pushViewController(vc, animated: true)
     }
+    
+    
 
+    @IBAction func logIntapped(_ sender: Any) {
+        
+        loginButtonTapped()
+    }
+    
+    func loginButtonTapped(){
+        
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+        
+        guard let email = emailField.text,
+              let password = passwordField.text,
+              !email.isEmpty, !password.isEmpty,
+              password.count >= 6 else {
+            showAlert()
+            return
+        }
+        
+        #warning("Firebase login need to be implemented here")
+    }
+    
+    
+    func showAlert(){
+        let ac = UIAlertController(
+            title: "Woops",
+            message: "Please enter all the information correctly to login",
+            preferredStyle: .alert
+        )
+        ac.addAction(
+            UIAlertAction(
+                title: "Wokay",
+                style: .cancel
+            )
+        )
+        present(ac, animated: true)
+    }
+}
+
+extension LoginViewController: UITextFieldDelegate{
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailField{
+            passwordField.becomeFirstResponder()
+        }else if textField == passwordField{
+            loginButtonTapped()
+        }
+        return true
+    }
+    
 }
--- ChatBox/Controllers/Login Rigistration/RegistrationViewController.swift
+++ ChatBox/Controllers/Login Rigistration/RegistrationViewController.swift
@@ -9,12 +9,95 @@ import UIKit
 
 class RegistrationViewController: UIViewController {
 
+    @IBOutlet weak var profilePic: UIImageView!
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Create account"
         view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        firstNameField.delegate = self
+        lastNameField.delegate = self
+        emailField.delegate = self
+        passwordField.delegate = self
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(editImageTapped))
+        profilePic.image = UIImage(systemName: "person.fill.badge.plus")
+        profilePic.tintColor = .gray
+        gesture.numberOfTapsRequired = 1
+        gesture.numberOfTouchesRequired = 1
+        profilePic.addGestureRecognizer(gesture)
+        if profilePic.image == UIImage(systemName: "person.fill.badge.plus"){
+            profilePic.layer.cornerRadius = 0
+        }else{
+            profilePic.layer.cornerRadius = 70
+        }
+        
     }
     
+    @objc func editImageTapped(){
+        print("hi")
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: Any) {
+        registerButtonTapped()
+    }
+    
+    func registerButtonTapped(){
+        
+        firstNameField.resignFirstResponder()
+        lastNameField.resignFirstResponder()
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+        
+        guard let email = emailField.text,
+              let password = passwordField.text,
+              let firstName = firstNameField.text,
+              let lastName = lastNameField.text,
+              !firstName.isEmpty,
+              !lastName.isEmpty,
+              !email.isEmpty,
+              !password.isEmpty,
+              password.count >= 6 else {
+            showAlert()
+            return
+        }
+        
+    }
+    func showAlert(){
+        let ac = UIAlertController(
+            title: "Woops",
+            message: "Please enter all the information correctly to register.",
+            preferredStyle: .alert
+        )
+        ac.addAction(
+            UIAlertAction(
+                title: "Wokay",
+                style: .cancel
+            )
+        )
+        present(ac, animated: true)
+    }
+    
+}
 
+extension RegistrationViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == firstNameField{
+            lastNameField.becomeFirstResponder()
+        }else if textField == lastNameField{
+            emailField.becomeFirstResponder()
+        }else if textField == emailField{
+            passwordField.becomeFirstResponder()
+        }else if textField == passwordField{
+            registerButtonTapped()
+        }
+        return true
+    }
 }
